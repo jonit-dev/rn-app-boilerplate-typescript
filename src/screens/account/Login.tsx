812ed9a4ae64159d0b9d74732e2592437c4db766
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-11-12 15:52:07 -0800

    unused styles check

---
+++ src/screens/account/Login.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Login extends Component {
+  public render() {
+    return (
+      <View style={styles.container}>
+        <Text>My New Component</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
