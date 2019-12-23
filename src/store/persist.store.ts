40b2b0838e5f71efe5e81e31caa9bf305ce963c6
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-12-23 12:51:08 -0800

    refactored persist configs to inside my index reduceR

--- src/store/persist.store.ts
+++ src/store/persist.store.ts
@@ -1,17 +1,8 @@
-import { AsyncStorage } from 'react-native';
 import { applyMiddleware, compose, createStore } from 'redux';
-import { persistReducer, persistStore } from 'redux-persist';
+import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers/index.reducers';
-
-const persistConfig = {
-  key: "root",
-  storage: AsyncStorage,
-  whitelist: ["userReducer"]
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
 
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -19,7 +10,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middlewares = [thunk];
 
 const store = createStore(
-  persistedReducer,
+  reducers,
   composeEnhancers(applyMiddleware(...middlewares))
 );
 
--- src/store/reducers/index.reducers.ts
+++ src/store/reducers/index.reducers.ts
@@ -1,12 +1,30 @@
+import { AsyncStorage } from 'react-native';
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 
 import { chatReducer } from './chat.reducer';
 import { uiReducer } from './ui.reducer';
 import { userReducer } from './user.reducer';
 
+const chatPersistconfig = {
+  key: "chatReducer",
+  storage: AsyncStorage,
+  whitelist: ["conversations"]
+};
+
+const rootReducerPersistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["userReducer"]
+};
+
 // tslint:disable-next-line: no-default-export
-export default combineReducers({
+const rootReducer = combineReducers({
   uiReducer,
   userReducer,
-  chatReducer
+  // @ts-ignore
+  chatReducer: persistReducer(chatPersistconfig, chatReducer)
 });
+
+// tslint:disable-next-line: no-default-export
+export default persistReducer(rootReducerPersistConfig, rootReducer);
