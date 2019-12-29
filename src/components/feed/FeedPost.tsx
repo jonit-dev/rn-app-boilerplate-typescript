cadf8ce868e5c47472b93036bcc10f61515b628c
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-12-28 22:33:14 -0800

    text margin

--- src/components/feed/FeedPost.tsx
+++ src/components/feed/FeedPost.tsx
@@ -105,6 +105,7 @@ const styles = StyleSheet.create({
   postText: {
     flex: 1,
     color: colors.silver,
-    fontSize: defaultFontSize
+    fontSize: defaultFontSize,
+    marginTop: 8
   }
 });
