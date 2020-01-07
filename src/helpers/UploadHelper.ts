7031ccb8baff72aaf591ff5ae948ba02258b81d4
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-01-06 23:25:44 -0800

    upload helper empty

--- src/helpers/UploadHelper.ts
+++ src/helpers/UploadHelper.ts
@@ -1,14 +1,1 @@
-export class UploadHelper {
-  // **dataURL to blob**
-  public static dataURLtoBlob = dataurl => {
-    let arr = dataurl.split(","),
-      mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]),
-      n = bstr.length,
-      u8arr = new Uint8Array(n);
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new Blob([u8arr], { type: mime });
-  };
-}
+export class UploadHelper {}
